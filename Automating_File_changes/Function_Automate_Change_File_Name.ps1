d2f9333877ad38977a9a15e515d7a97c18e5dcc4
Richard Roelofs <richardroelofs@gmail.com>
2022-11-06 14:34:15 +0100

    Function Written

--- Automating_File_changes/Function_Automate_Change_File_Name.ps1
+++ Automating_File_changes/Function_Automate_Change_File_Name.ps1
@@ -1,17 +1,37 @@
-# Set default location for working with files and folder @root folder
-Set-Location $PSScriptRoot
+Function New-FileName{
+    [cmdletbinding()]
+    param(
+        [string]$FilePath = ".\Images"
+    )
 
-# list
-
-$Names = Get-ChildItem -Path ".\images\*.jpg" | Select-Object -ExpandProperty Name
-
-
-foreach($name in $names[0]){
-    $newName = $name | select-string -Pattern "\d{6}" | Select-Object -expandProperty Matches | Select-Object -Property Value
-    $newName
+    BEGIN{
+        Write-Verbose "[BEGIN]: Starting Function $($MyInvocation.MyCommand)"
+        Set-Location $PSScriptRoot
+        Write-Verbose "[BEGIN]: Execute Set-Location for default : $PSScriptRoot"
+        Write-verbose "[BEGIN]: Explain: copy folder with name Images and content to above location!"
+    }
     
-    Rename-Item -Path ".\images\$name" -NewName "$($newName.Value).jpg" -Verbose
-    
-    #Use in Function
-    #Write-Verbose "$((Get-Date).DateTime):New name for $name is $newname"
+    PROCESS{
+        Write-Verbose "[PROCESS]: Retrieve the filenames"
+        $names = Get-ChildItem -Path "$FilePath\*.jpg" | Select-Object -ExpandProperty Name
+        Write-Verbose "[PROCESS]: $($names.count) files will be renamed"
+
+        #loop for making the change 
+        foreach($name in $names){
+            $newName = $name | select-string -Pattern "\d{6}" | Select-Object -ExpandProperty Matches | Select-Object -Property Value
+            Rename-Item -Path ".\images\$name" -NewName "$($newName.Value).jpg" -InformationAction Continue -InformationVariable i
+            
+            Write-Information -MessageData "$((Get-Date).DateTime):New name for $name is $($newname.Value).jpg" -InformationAction Continue
+          
+            
+
+        }
+        
+    }
+    END{
+        Write-Verbose "[END]: Filenames are renamed. See folder log for results"
+    }
+
+
 }
+New-FileName -Verbose -InformationAction Continue 6> ".\log\newNameLog.txt"
